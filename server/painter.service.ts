34da5036d6ed09f46df2fa3b337cf39619bd0e14
Laudrup69 <laudrup69@gmail.com>
2016-07-28 09:29:12 +0200

    update

--- server/painter.service.ts
+++ server/painter.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@angular/core';
 
-import { Painter } from './Painter';
-import { PAINTERS } from './server/painters.mock';
+import { Painter } from '../Painter';
+import { PAINTERS } from './painters.mock';
+
+@Injectable()
+export class PainterService{
+
+    getPainters(): Promise<Painter[]>
+    {
+        return Promise.resolve(PAINTERS);
+    }
+
+    getLaggedPainters(): Promise<Painter[]>
+    {
+        return new Promise<Painter[]>(resolve =>
+            setTimeOut(()=>resolve(PAINTERS), 3000));
+    }
+}
