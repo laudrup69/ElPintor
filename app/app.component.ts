7bf2588358da72d06fcca46696d22900d6dd7064
Laudrup69 <laudrup69@gmail.com>
2016-07-29 10:44:47 +0200

    Modificación de estructura de carpetas

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Painter } from './painter';
+import { PainterDetailComponet } from './painter-detail.component';
+import { PainterService } from './server/painter.service';
+
+@Component({
+    directives: [PainterDetailComponet],
+    providers: [PainterService],
+    selector: 'my-app',
+    template: `
+        <h1><img class="logoImg" src="img/brush.png" /> Famous painters</h1>
+        <ul class="painters">
+            <li *ngFor="let p of painters"
+                [class.selected]="p === selectedPainter"
+                (click)="onSelect(p)">
+                <span class="badge">{{p.id}}</span> {{p.name}}
+            </li>
+        </ul>
+        <my-painter-detail [painter]="selectedPainter"></my-painter-detail>
+    `
+})
