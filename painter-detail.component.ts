f8c73181634560460bdcae62b0b2cf7d4b888327
Laudrup69 <laudrup69@gmail.com>
2016-07-29 10:33:03 +0200

    Detail painter

---
+++ painter-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { Painter } from './painter';
+
+@Component({
+    inputs: ['painter'],
+    selector: 'my-painter-detail',
+    template: `
+        <div *ngIf="painter">
+            <h2>{{painter.name}}</h2>
+            <div><label>style: </label>{{painter.style}}</div>
+            <div><label>examples: </label>{{painter.examples}}</div>
+            <div>
+                <h3>Edit</h3>
+                <label>name: </label>
+                <div><input [(ngModel)]="painter.name" placeholder="name"></div>
+            </div>
+        </div>
+    `
+})
+
+export class PainterDetailComponent{
+    public painter: Painter;
+}
